04b671f48a943aae3443625e15626db67777bacb
Madan Mohan Kumar <85798267+tuntun10@users.noreply.github.com>
2026-06-25 01:18:14 +0530

    Add files via upload

---
+++ Add.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Add = () => {
+const user = {
+  username:"madan",
+  age:30,
+  city:"mumbai"
+}
+
+  return (
+    <div>
+      <h2>userinfo</h2>
+      <p>user.username</p>
+      <p>user.age</p>
+      <p>user.city</p>
+    </div>
+  )
+}
+
+export default Add
